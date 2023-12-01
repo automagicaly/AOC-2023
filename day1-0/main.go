7d192f13c06ad6977561401449786bb2bc9aa794
Renan Lordello de Aguiar <renan.lord.de.aguiar@gmail.com>
2023-12-01 14:52:38 -0300

    Day 1 Question 0

---
+++ day1-0/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+    "os"
+)
+
+func main() {
+    reader := bufio.NewScanner(os.Stdin)
+    calibration := 0
+    for reader.Scan() {
+        fd := -1
+        ld := -1
+        for _, r := range reader.Text() {
+            if r >= '0' && r <= '9' {
+                if fd == -1 {
+                    fd = int(r - '0')
+                }
+                ld = int(r - '0')
+            }
+        }
+        calibrationValue := (fd*10 + ld)
+        calibration += calibrationValue
+        fmt.Printf("%d\n", calibrationValue)
+    }
+    fmt.Printf("%d\n", calibration)
+}
